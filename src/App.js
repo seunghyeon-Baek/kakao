86b0e8e0b4f2b9b3c3a24d929c782ef834e496ab
seunghyeon <vjvmf123456@gmail.com>
2023-11-30 18:11:50 +0900

    maintop생성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './assets/css/common.scss'
 import './App.css';
+import MainTop from './Components/Main/MainTop';
 
 
 function App() {
   return (
-      <div className=''>
-        백승현입니다.
-      </div>
+      // <div className=''>
+        // 백승현입니다.
+        <MainTop />
+      // </div>
+
   );
 }
 
---
+++ src/Components/Main/MainTop.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../assets/css/_mainTop.scss'
+import '../../assets/css/common.scss'; 
+
+function MainTop() {
+  return (
+    <div className="main">
+      <div className='cotent' >
+       <div className='title_date'>
+            test
+       </div>
+      </div>
+    </div>
+  );
+}
+
+export default MainTop;
