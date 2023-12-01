9d905e2f3cd299750a0bed6360d22dd7df592c2e
hiptae <rlagmlxo812@gmail.com>
2023-12-01 16:53:09 +0900

    hiptae

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './assets/css/common.scss'
 import './App.css';
 import Header from './Components/Header/Header'
 import MainTop from './Components/Main/MainTop';
+import MainNews from './Components/Main/MainNews';
 
 function App() {
   return (
       <div className=''>
         <Header/>
         <MainTop />
+        <MainNews/>
       </div>
 
 
