f324570311006805eb81b29eebd03ff2276c8881
hiptae <142563255+hiptae@users.noreply.github.com>
2023-12-01 11:26:12 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import logo from './logo.svg';
 import './assets/css/common.scss'
 import './App.css';
-
+import Header from './Components/Header/Header'
 import MainTop from './Components/Main/MainTop';
 
 function App() {
   return (
       // <div className=''>
+        <Header/>
         // 백승현입니다.
         <MainTop />
       // </div>
-import Header from './Components/Header/Header'
 
-      <Header/>
+
+      
 
   );
 }
